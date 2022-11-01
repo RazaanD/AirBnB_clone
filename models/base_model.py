e92df23d6a7867a335e4f98ac4e8074b8cf9f24b
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-11-01 23:50:25 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,62 +1,50 @@
 #!/usr/bin/python3
-""" BaseModel class. Used as base for future classes.
-"""
+""" Class BaseModel """
 from datetime import datetime
 from uuid import uuid4
 import models
 
 
-class BaseModel():
-    """ BaseModel class. Used as base for future classes.
-    """
+class BaseModel:
+    """ construct """
+
     def __init__(self, *args, **kwargs):
-        """Validate non interactive and init method
-        """
-        if len(kwargs) is not 0:
+        """ Construct """
+        if kwargs:
             for key, value in kwargs.items():
-                if key == "id":
-                    self.id = value
-                    continue
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        value, '%Y-%m-%dT%H:%M:%S.%f')
-                    continue
-                if key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        value, '%Y-%m-%dT%H:%M:%S.%f')
-                    continue
                 if key == '__class__':
                     continue
+                elif key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == 'created_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if 'id' not in kwargs.keys():
+                    self.id = str(uuid4())
+                if 'created_at' not in kwargs.keys():
+                    self.created_at = datetime.now()
+                if 'updated_at' not in kwargs.keys():
+                    self.updated_at = datetime.now()
                 setattr(self, key, value)
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            """if it’s a new instance (not from a dictionary representation),
-            add a call to the method new(self) on storage
-            """
+            self.updated_at = self.created_at
             models.storage.new(self)
 
     def __str__(self):
-        """Return the string representation
-        """
-        return ("[{}] ({}) {}".format(self.__class__.__name__,
-                                      self.id, self.to_dict()))
+        """ String """
+        return('[' + type(self).__name__ + '] (' + str(self.id) +
+                ') ' + str(self.__dict__))
 
     def save(self):
-        """Update date
-        """
+        """ save function """
         self.updated_at = datetime.now()
-        """In the method save(self):
-        call save(self) method of storage
-        """
         models.storage.save()
 
     def to_dict(self):
-        """ Return the dictionary
-        """
-        _dict = self.__dict__.copy()
-        _dict['created_at'] = self.created_at.isoformat()
-        _dict['updated_at'] = self.updated_at.isoformat()
-        _dict['__class__'] = self.__class__.__name__
-        return(_dict)
+        """ Return a dictonary """
+        aux_dict = self.__dict__.copy()
+        aux_dict['__class__'] = self.__class__.__name__
+        aux_dict['created_at'] = self.created_at.isoformat()
+        aux_dict['updated_at'] = self.updated_at.isoformat()
+        return aux_dict
