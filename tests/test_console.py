7ceaf1b0df48d17f66f5802182240a2bcba78b12
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-11-01 23:29:55 +0200

    Create test_console.py

---
+++ tests/test_console.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+"""Testing file to test console
+"""
+
+import unittest
+from unittest.mock import patch
+from io import StringIO
+import json
+import pep8
+import console
+import os
+import test
+# Import console
+from console import HBNBCommand
+# Import models
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.engine.file_storage import FileStorage
+
+
+class TestConsole(unittest.TestCase):
+    """Create testcases for the console
+    """
+
+    # First things first, the setupclass
+    @classmethod
+    def setUpClass(cls):
+        """Called before test in an individual class are run (HHNBCommand)
+        """
+        cls.consol = HBNBCommand()
+
+    @classmethod
+    def tearDown(cls):
+        """Called after test in an individual class have run (del HHNBCommand)
+        """
+        del cls.console
+
+    # Clean after run tests
+    def tearDown(self):
+        """Clean file created after run the test (json file)
+        """
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
+
+    def test_docstrings_in_console(self):
+        """checking for docstrings"""
+        self.assertIsNotNone(console.__doc__)
+        self.assertIsNotNone(HBNBCommand.emptyline.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_quit.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_EOF.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_create.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_show.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_destroy.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_all.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_update.__doc__)
+        self.assertIsNotNone(HBNBCommand.default.__doc__)
+
+    def test_quit(self):
+        """test quit
+        """
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.onecmd("quit")
+            self.assertEqual('', f.getvalue())
+
+    def test_create(self):
+        """Test create command
+        """
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.onecmd("create")
+            self.assertEqual(
+                "** class name missing **\n", f.getvalue())
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.onecmd("create asdfsfsd")
+            self.assertEqual(
+                "** class doesn't exist **\n", f.getvalue())
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.onecmd("create User")
+
+
+if __name__ == "__main__":
+    unittest.main()
