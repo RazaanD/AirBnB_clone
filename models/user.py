d2b9aa50ff40641de73a90bb6c0cee51b5c34252
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-11-01 23:53:27 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-""" User class. Used as base for future users."""
-
+""" User Class """
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """ User class. Used as base for future users."""
-
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    """ User class that inherits BaseModel """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
